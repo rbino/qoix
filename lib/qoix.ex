b8d8174ce8b563d6033692e95bf351db8ae7d47c
Riccardo Binetti <rbino@gmx.com>
2021-11-28 23:50:16 +0100

    Adapt header format to new specification
    See https://github.com/phoboslab/qoi/issues/37
    
    Signed-off-by: Riccardo Binetti <rbino@gmx.com>

--- lib/qoix.ex
+++ lib/qoix.ex
@@ -3,13 +3,12 @@ defmodule Qoix do
   Qoix is an Elixir implementation of the [Quite OK Image](https://phoboslab.org/log/2021/11/qoi-fast-lossless-image-compression) format.
   """
 
-  @header_size 12
 
   @doc """
   Returns true if the binary appears to contain a valid QOI image.
   """
-  def qoi?(<<"qoif", _width::size(16), _height::size(16), size::size(32), _::binary>> = binary)
-      when byte_size(binary) >= @header_size + size do
+  def qoi?(<<"qoif", _width::32, _height::32, channels::8, _colorspace::8, _rest::binary>>)
+      when channels == 3 or channels == 4 do
     true
   end
 
